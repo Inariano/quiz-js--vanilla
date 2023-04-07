3735f61c5e7a83b23b9ebb75c2f27f76df5063a2
Inariano <86901316+Inariano@users.noreply.github.com>
2023-04-07 15:38:57 +0000

    crear array de questoes

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert("rodando js ")
+
+////array de perguntas//////
+
+const Questions =[ 
+                   {question:"1+0", 
+                    answers:[
+                     {answer:"1", correct: true },
+                     {answer:"2", correct: false },
+                     {answer:"3", correct: false },
+                    ]},
+                   {question:"1+1", 
+                    answers:[
+                     {answer:"1", correct: false},
+                     {answer:"2", correct: true  },
+                     {answer:"3", correct: false},
+                    ]},
+                   {question:"1+2", 
+                    answers:[
+                     {answer:"1", correct: false},
+                     {answer:"2", correct: false },
+                     {answer:"3", correct: true },
+                    ]},
+                 ]
+
+                 
